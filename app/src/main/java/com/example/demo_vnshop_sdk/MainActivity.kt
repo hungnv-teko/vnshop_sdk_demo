19c3db9c9aeb5df7c792bcccaefa39ca93dccf25
tunb-teko <tu.nb@teko.vn>
2020-11-30 14:15:15 +0700

    Update dependencies and id_token

--- app/src/main/java/com/example/demo_vnshop_sdk/MainActivity.kt
+++ app/src/main/java/com/example/demo_vnshop_sdk/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import vn.teko.android.auth.core.TerraAuth
 import vn.teko.android.auth.login.TerraLogin
 import vn.teko.android.auth.login.provider.AUTH_MANAGER_EXTRA_CUSTOM_TOKEN_IDTOKEN
@@ -38,17 +39,20 @@ class MainActivity : AppCompatActivity() {
         val data = Intent().apply {
             putExtra(
                     AUTH_MANAGER_EXTRA_CUSTOM_TOKEN_IDTOKEN,
-                    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjBkNTdhMGJlLTNlZDItNDJhMi1iM2U1LTM2Y2E3ZjkzMjEyMjQifQ.eyJzdWIiOjExMjM2OCwibmFtZSI6IlR14bqlbiBOZ3V54buFbiIsImVtYWlsIjoidHVhbkBnbWFpbC5jb20iLCJwaG9uZV9udW1iZXIiOiIwOTg3NjYzOTY3IiwicGljdHVyZSI6Imh0dHBzOi8vYXNzZXRzLmljaGVjay52bi9tdWx0aXBhcnQvMjAyMC9hcHAwMS85LzhhN2IwY2Y4NjFhOGQ1YzgwYjQzZTUzMWFkNjljODM0LnBuZyIsImFkZHJlc3MiOiIzNjggQ-G6p3UgR2nhuqV5IiwiYmlydGhkYXkiOiIxMi8yLzIwMDAiLCJhdWQiOiJpY2hlY2siLCJpYXQiOjE2MDY0NDg4ODQsImV4cCI6MTYwNjYyMTY4NCwiaXNzIjoibXlpc3N1ZXJuYW1lIn0.OncjCx0mYiGrTBbfyXKzGmGQWoelB11T5JO9oAYOFLtxxMkKBLkYszT2beEST8kKyFshdJ1HrkCIb25vAaeD3Q"
+                    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjBkNTdhMGJlLTNlZDItNDJhMi1iM2U1LTM2Y2E3ZjkzMjEyMjQifQ.eyJzdWIiOjExMjM2OCwibmFtZSI6IlR14bqlbiBOZ3V54buFbiIsImVtYWlsIjoidHVhbkBnbWFpbC5jb20iLCJwaG9uZV9udW1iZXIiOiIwOTg3NjYzOTY3IiwicGljdHVyZSI6Imh0dHBzOi8vYXNzZXRzLmljaGVjay52bi9tdWx0aXBhcnQvMjAyMC9hcHAwMS85LzhhN2IwY2Y4NjFhOGQ1YzgwYjQzZTUzMWFkNjljODM0LnBuZyIsImFkZHJlc3MiOiIzNjggQ-G6p3UgR2nhuqV5IiwiYmlydGhkYXkiOiIxMi8yLzIwMDAiLCJhdWQiOiJpY2hlY2siLCJpYXQiOjE2MDY3MTk4MDIsImV4cCI6MTYwNjg5MjYwMiwiaXNzIjoibXlpc3N1ZXJuYW1lIn0.SpUqKWx3FPyzhjiyvjGcABc3E2Wj3TpCXuVPf4TQlnjD97V-yHwoHwdGbhOa9FSsWMfk8W-lKQtWy_eMCGWfvg"
             )
             putExtra(AUTH_MANAGER_EXTRA_CUSTOM_TOKEN_PROVIDER, "icheck")
         }
         loginManager.processLoginResult(AUTH_MANAGER_RC_LOGIN, Activity.RESULT_OK, data) {result ->
             when(result) {
                 is Result.Success -> {
-                    //Login Success
+                    AlertDialog.Builder(this).setMessage("Login successfully").setPositiveButton("ok") { _, _ -> }
+                        .show()
                 }
                 is Result.Failure -> {
-                    //Login Failure
+                    val error = result.exception()
+                    AlertDialog.Builder(this).setMessage("Login error: $error").setPositiveButton("ok") { _, _ -> }
+                        .show()
                 }
             }
         }
